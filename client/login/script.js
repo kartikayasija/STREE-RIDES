594e38c4e9e7ac0205c4244f066531411214832a
Kartikay Asija <kartikay7057@gmail.com>
2023-04-24 01:06:35 +0530

    button changed

--- client/login/script.js
+++ client/login/script.js
@@ -67,7 +67,7 @@ const signup=()=>{
 
   console.log(name,contact,email,password);
 
-  fetch("http://localhost:3000/auth/signup", {
+  fetch("https://stree-rides.vercel.app/auth/signup", {
     method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
